f5c247bf4a018f190d4515df91ef9bfb1d8914d3
the-menna-sherif <mennaseducation@gmail.com>
2026-03-07 15:33:27 +0200

    Created create and post APIs and schema

--- app/app.py
+++ app/app.py
@@ -1,11 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
 
 app = FastAPI()
 
 # Create endpoint & method
 
-# create a   for the endpoint 
-@app.get("/hello-world")
-def hello_world():
-    # return any data in either a dictionary (below) or a pydantic object
-    return {"message": "Hello World"}
+# # create a   for the endpoint 
+# @app.get("/hello-world")
+# def hello_world():
+#     # return any data in either a dictionary (below) or a pydantic object
+#     return {"message": "Hello World"}
+
+# container dict for our text posts
+text_posts = {
+    "1": {"title": "New Post", "content": "Cool test post"},
+    "2": {"title": "Python Tip", "content": "Use list comprehensions for cleaner loops."},
+    "3": {"title": "Daily Motivation", "content": "Consistency beats intensity every time."},
+    "4": {"title": "Fun Fact", "content": "The first computer bug was an actual moth found in a Harvard Mark II."},
+    "5": {"title": "Update", "content": "Just launched my new project! Excited to share more soon."},
+    "6": {"title": "Tech Insight", "content": "Async IO in Python can massively speed up I/O-bound tasks."},
+    "7": {"title": "Quote", "content": "Programs must be written for people to read, and only incidentally for machines to execute."},
+    "8": {"title": "Weekend Plans", "content": "Might finally clean up my GitHub repos... or just play some Minecraft."},
+    "9": {"title": "Question", "content": "What’s the most underrated Python library you’ve ever used?"},
+    "10": {"title": "Mini Announcement", "content": "New video drops tomorrow—covering the weirdest Python features!"}
+}
+
+# Our READ endpoint to get all text posts
+# added an optional (using syntax: = None) query parameter to specify the number of posts to return
+# can pass many params (comma separated) 
+@app.get("/posts")
+def get_all_posts(limit: int = None):
+    if limit:
+        return list(text_posts.values())[:limit]
+    return text_posts
+
+# Our READ endpoint to get a single text post by id using its path parameter
+@app.get("/posts/{id}")
+def get_post(id: str):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return text_posts.get(id)
+
+@app.post("/posts") 
+# receiving the request body as a pydantic model (PostCreate) which will validate the incoming data
+def create_post(post: PostCreate):
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()+1)] = new_post
+    return new_post
