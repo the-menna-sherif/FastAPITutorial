7774dafd4021b14279fb534ed2abc0b2b9301312
the-menna-sherif <mennaseducation@gmail.com>
2026-03-14 14:36:41 +0200

    Title added to app and commented GET fxns

--- ErrorHandlingTutorial/app/app.py
+++ ErrorHandlingTutorial/app/app.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Simple FastAPI Gateway")
 
-# decorator extending my function to be a GET request
-@app.get("/")
-def read_root():
+
+@app.get("/") # decorator extending my function to be a GET request
+def read_root(): # fxn called when root endpoint gets called by GET request
     return {"message": 
             "Hello World!"}
 
-@app.get("/")
-def example1():
+@app.get("/example") # decorator extending my function to be a GET request
+def example1(): # fxn called when /example endpoint gets called by GET request 
   return {"message":
           "My example works!"}
+
+
